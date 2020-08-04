488fecf0b8f14101ff71bdffd1b1a1b23e3a0d35
maywzh <maywzh@gmail.com>
2020-08-04 15:19:17 +0800

    treemap

--- collection/treeMap/Main.java
+++ collection/treeMap/Main.java
@@ -1,15 +1,34 @@
-import java.util.TreeMap;
-import java.util.Map;
+import java.rmi.StubNotFoundException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, Integer> map = new TreeMap<>();
-        map.put("orange", 1);
-        map.put("apple", 2);
-        map.put("pear", 3);
-        for (String key : map.keySet()) {
+        Map<Student, Integer> map = new TreeMap<>(new Comparator<Student>() {
+            public int compare(Student s1, Student s2) {
+                return s1.score > s2.score ? -1 : s1.score == s2.score ? 0 : 1;
+            }
+
+        });
+        map.put(new Student("Tom", 77), 1);
+        map.put(new Student("Bob", 66), 2);
+        map.put(new Student("Lily", 99), 3);
+        for (Student key : map.keySet()) {
             System.out.println(key);
         }
-        // apple, orange, pear
+        System.out.println(map.get(new Student("Bob", 66))); // null?
+    }
+}
+
+class Student {
+    public String name;
+    public int score;
+
+    Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String toString() {
+        return String.format("{%s: score=%d}", name, score);
     }
 }
