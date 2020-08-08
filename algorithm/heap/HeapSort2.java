bf51c2e9560cf429a5ebe58c9b97a27d2c729b85
maywzh <maywzh@gmail.com>
2020-08-08 19:12:52 +0800

    heap

---
+++ algorithm/heap/HeapSort2.java
@@ -0,0 +1,43 @@
+package sortdemo;
+
+import java.util.Arrays;
+
+public class HeapSort2 {
+    public static void main(String[] args) {
+        int[] arr = { 9, 8, 6, 7, 5, 3, 4, 1, 2 };
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void sort(int arr[]) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, i, 0);
+            heapify(arr, 0, i);
+        }
+    }
+
+    public static void heapify(int arr[], int i, int length) {
+        int temp = arr[i];
+        for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {
+            if (k + 1 < length && arr[k] < arr[k + 1]) {
+                k++;
+            }
+            if (arr[k] > temp) {
+                arr[i] = arr[k];
+                i = k;
+            } else {
+                break;
+            }
+        }
+        arr[i] = temp;
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
