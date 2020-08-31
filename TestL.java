a9e0e67f970cf9e54f8796f67cd36050e0a2980d
maywzh <maywzh@gmail.com>
2020-08-31 21:41:49 +0800

    test

---
+++ TestL.java
@@ -0,0 +1,15 @@
+/**
+ * TestL
+ */
+public class TestL {
+
+    public static void main(String[] args) {
+        try {
+            String st = "java开发";
+        System.out.println(st.getBytes("UTF-8").length);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+    }
+}
---
+++ javabase/MyClass.java
@@ -0,0 +1,8 @@
+public class MyClass {
+
+    static int i;
+
+    public static void main(String argv[]) {
+        System.out.println(i);
+    }
+}
