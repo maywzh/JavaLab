dafd05c8aaef6561e7a2b734b3d360df6d040ad4
maywzh <maywzh@gmail.com>
2020-09-07 12:16:32 +0800

    g

--- common/IO.go
+++ common/IO.go
@@ -4,13 +4,16 @@ import "fmt"
 
 func main() {
 	var m, n int
+	var x, y int
 	fmt.Scanln(&m, &n)
-	var hash [][]int = make([][]int, m)
-	for i := 0; i < m; i++ {
-		hash[i] = make([]int, n)
-		for j := 0; j < n; j++ {
-			fmt.Scan(&hash[i][j])
-		}
-	}
-	fmt.Println(hash)
+	fmt.Scanln(&x, &y)
+	// var hash [][]int = make([][]int, m)
+	// for i := 0; i < m; i++ {
+	// 	hash[i] = make([]int, n)
+	// 	for j := 0; j < n; j++ {
+	// 		fmt.Scan(&hash[i][j])
+	// 	}
+	// }
+	fmt.Println(m, n, x, y)
+	// fmt.Println(hash)
 }
